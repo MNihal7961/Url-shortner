4b7f06ff75f13f8082c33d58ca1e56bd295f7a30
M Nihal <mnihal@Ms-MacBook-Air.local>
2024-05-06 16:18:04 +0530

    Mongodb connection setted

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import dbConnect from "./config/db";
 
 dotenv.config()
 
@@ -10,7 +11,11 @@ app.use(express.urlencoded({ extended: true }))
 
 const PORT: number | string = process.env.PORT || 5000
 
-const server = app.listen(PORT, () => console.log("Server up and running at port:", PORT))
+// Server connection
+const server = app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+
+// DB connection
+dbConnect()
 
 
 process.on('unhandledRejection', (reason, p) => {
